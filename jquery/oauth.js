9b14556b445103d309445034ab825d21994dfc1d
Jakob Buis <jpabuis@gmail.com>
2014-05-11 17:54:27 +0200

    renamed config to the more industry-standard options

--- jquery/oauth.js
+++ jquery/oauth.js
@@ -1,4 +1,4 @@
-var OAuth = (function(config){
+var OAuth = (function(options){
 
     var getURLParameter = function (name) {
         return decodeURI(
@@ -8,38 +8,37 @@ var OAuth = (function(config){
 
     var access_token = null;
     var authorization_token = null;
-    var config = config;
 
     return {
         authenticate: function(callback){
             var authorization_token = getURLParameter('code');
             if (authorization_token === 'null') {  // Yes, this is correct
                 // Not authenticated, must login
-                window.location = config.url+'authorize?response_type=code&client_id='+config.client+'&client_pass='+config.secret+'&redirect_uri='+config.callback+'&state=1';
+                window.location = options.url+'authorize?response_type=code&client_id='+options.client+'&client_pass='+options.secret+'&redirect_uri='+options.callback+'&state=1';
             }
             else {
                 // Logged in, request access_token to access services
                 $.ajax({
                     method: 'POST',
-                    url: config.url+'token',
+                    url: options.url+'token',
                     dataType: 'JSON',
                     data: {
                         grant_type: 'authorization_code',
                         code: authorization_token,
-                        redirect_uri: config.callback,
-                        client_id: config.client,
-                        client_secret: config.secret,
+                        redirect_uri: options.callback,
+                        client_id: options.client,
+                        client_secret: options.secret,
                     },
                     success: function(result){
                         // Store the access token for later usage
                         this.access_token = result.access_token;
                         // Clear the browser URL for cleaner reloads
-                        history.pushState(null, '', config.callback);
+                        history.pushState(null, '', options.callback);
 
                         // Check for authorization
                         $.ajax({
                             method: 'GET',
-                            url: config.endpoint+config.resource+'?access_token='+this.access_token,
+                            url: options.endpoint+options.resource+'?access_token='+this.access_token,
                             success: callback(true),
                         });
                     },
